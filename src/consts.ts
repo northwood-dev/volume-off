ae563ab1aabfb2a42a4b8ed369cd5aca8350f14e
axelbaron <axelbaron@outlook.fr>
2026-04-29 17:48:20 +0200

    chore: rebrand Groove Lab Media → VOLUME OFF

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Groove Lab Media';
-export const SITE_DESCRIPTION = 'Fueling your digital rhythm! Discover new groove music and captivating content.';
+export const SITE_TITLE = 'VOLUME OFF';
+export const SITE_DESCRIPTION = 'Turning down thw noise to hear the music.';
