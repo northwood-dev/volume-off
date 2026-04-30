946dbcabe7c1fd768b7ece156966dc1f3834460f
axelbaron <axelbaron@outlook.fr>
2026-04-30 19:44:54 +0200

    test fly

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com', // TODO, change that later
+	site: 'https://volumeoff.io',
 	integrations: [mdx(), sitemap()],
 	redirects: {
 		"/": "/blog/"
